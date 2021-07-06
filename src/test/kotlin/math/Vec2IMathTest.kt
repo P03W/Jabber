a5f022dd65d6214ab5559bc5ec3baad7ffb8147d
P03W <P03W@users.noreply.github.com>
2021-07-05 23:34:04 -0400

    This shouldn't be this fun (Vec2I unit tests)

---
+++ src/test/kotlin/math/Vec2IMathTest.kt
@@ -0,0 +1,36 @@
+package math
+
+import mc.jabber.math.Cardinal
+import mc.jabber.math.Vec2I
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Vec2IMathTest {
+    val vecA = Vec2I(2, -3)
+    val vecB = Vec2I(3, -3)
+
+    @Test
+    fun testVecComparison() {
+        assert(vecB > vecA)
+        assert(vecB >= vecA)
+        assert(vecA < vecB)
+        assert(vecA <= vecB)
+    }
+
+    @Test
+    fun testVecSelfMath() {
+        assert(vecA + vecB == Vec2I(5, -6))
+        assert(vecA - vecB == Vec2I(-1, 0))
+        assert(vecA * vecB == Vec2I(6, 9))
+        assert(vecA / vecB == Vec2I(2/3, 1))
+    }
+
+    @Test
+    fun testVecCardinalMath() {
+        assert(vecA + Cardinal.UP == vecA - Cardinal.DOWN)
+        assert(vecA + Cardinal.LEFT == vecA - Cardinal.RIGHT)
+
+        assert(vecA * Cardinal.UP == Vec2I(0, 3))
+        Assertions.assertThrows(ArithmeticException::class.java) { assert(vecA / Cardinal.UP == Vec2I(2, 0)) }
+    }
+}
