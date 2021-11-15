ec42d537689be8f07e2943f74c7dbec99f38ffe8
P03W <P03W@users.noreply.github.com>
2021-11-14 20:13:18 -0500

    Remove deps

--- src/main/kotlin/mc/jabber/init/Main.kt
+++ src/main/kotlin/mc/jabber/init/Main.kt
@@ -1,10 +1,7 @@
 package mc.jabber.init
 
 import mc.jabber.Global
-import mc.jabber.core.asm.CompiledCircuit
-import mc.jabber.core.data.serial.NbtTransformable
 import net.fabricmc.api.ModInitializer
-import kotlin.time.ExperimentalTime
 
 @Suppress("unused")
 object Main : ModInitializer {
