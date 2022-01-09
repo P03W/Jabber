4122629fb21cb667fbd39a4ee091b03c387c7ded
P03W <P03W@users.noreply.github.com>
2022-01-09 15:42:50 -0500

    ChipParams serialization

--- src/main/kotlin/mc/jabber/core/chips/ChipParams.kt
+++ src/main/kotlin/mc/jabber/core/chips/ChipParams.kt
@@ -2,6 +2,9 @@ package mc.jabber.core.chips
 
 import it.unimi.dsi.fastutil.objects.Object2LongArrayMap
 import mc.jabber.util.assertType
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtInt
+import net.minecraft.nbt.NbtLong
 
 class ChipParams(orig: ChipParams? = null, builder: ChipParams.()->Unit) {
     val longParams = Object2LongArrayMap<String>()
@@ -24,16 +27,38 @@ class ChipParams(orig: ChipParams? = null, builder: ChipParams.()->Unit) {
     fun registerLong(name: String) {
         longParams[name] = 0
     }
-
-    fun <E : Enum<E>> registerEnum(name: String, enum: E) {
-        enumParams[name] = enum
-    }
-
     fun getLong(name: String): Long {
         return longParams.getLong(name)
     }
 
+    fun <E : Enum<E>> registerEnum(name: String, enum: E) {
+        enumParams[name] = enum
+    }
     inline fun <reified E : Enum<E>> getEnum(name: String): E {
         return enumParams[name].assertType()!!
+    }
+
+    fun writeToNbt(): NbtCompound {
+        val nbt = NbtCompound()
+        longParams.forEach { (name, value) ->
+            nbt.putLong(name, value)
+        }
+        enumParams.forEach { (name, value) ->
+            nbt.putInt(name, value.ordinal)
+        }
+        return nbt
+    }
+
+    companion object {
+        fun fromNbt(nbt: NbtCompound): ChipParams {
+            return ChipParams {
+                nbt.keys.forEach { key ->
+                    when (val container = nbt.get(key)) {
+                        is NbtLong -> longParams.computeIfPresent(key) { _, _ -> container.longValue() }
+                        is NbtInt -> enumParams.computeIfPresent(key) { _, exiting -> exiting.javaClass.enumConstants[container.intValue()] }
+                    }
+                }
+            }
+        }
     }
 }
